3a8b497fdbdd9b2ababf9f1114e4b93448dced02
Joshua Lesko <77692625+leskojosh000@users.noreply.github.com>
2021-02-01 17:43:37 -0800

    Add files via upload

---
+++ sketch[1].js
@@ -0,0 +1,13 @@
+function setup() {
+ createCanvas(800, 300);
+ background(220,220,220);
+  rectMode(CENTER);
+}
+function draw() {
+  background(220);
+ rect(400, 150, 100, 100);
+  fill(136,86,167);
+  ellipse(350,120,100,100);
+  fill(158,188,218);
+  line(1,1,1000,300);
+}
